8449f1e4d152b4a1ec91d0060bc0488c73fea02a
unknown <ewerton.limasouza@gmail.com>
2021-05-18 11:31:21 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>ELLS info</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+  </head>
+  <body>
+      <h1 class="texto">
+    Aqui vai o código HTML que fará seu site aparecer.
+</h1>
+  </body>
+</html>
